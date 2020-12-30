e2d119b0ab95b8c378d10337b703d1e934cbb85e
Vinay <vinay.jaiswal@inmobi.com>
2020-12-31 00:30:31 +0530

    Correcting the pg-tailnmail config for new bin

--- pg-tailnmail/opt/vinay/dbops/pg-tailnmail/bin/postgres_error_mail.sh
+++ pg-tailnmail/opt/vinay/dbops/pg-tailnmail/bin/postgres_error_mail.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-LOCK="/opt/vinay/dbops/pg_tailnmail/cfg/.pg_tailnmail.lock"
+LOCK="/opt/vinay/dbops/pg-tailnmail/cfg/.pg-tailnmail.lock"
 PRG=$(basename $0)
 BOX=`uname -a | awk '{print $2}'`
-LOG="/opt/vinay/dbops/log/pg_tailnmail/postgres_error_mail.log"
+LOG="/opt/vinay/dbops/log/pg-tailnmail/postgres_error_mail.log"
 MailTo="jaiswalvinny9@gmail.com"
 MailError="jaiswalvinny9@gmail.com"
 Call="DB-Oncall"
 
-if [ -f "/opt/vinay/dbops/pg_tailnmail/cfg/tnm.config.txt" ];then
+if [ -f "/opt/vinay/dbops/pg-tailnmail/cfg/tnm.config.txt" ];then
 	if [ -f "$LOCK" ];then
 		if [ "$(ps -p `cat $LOCK` | wc -l)" -gt 1 ]; then
 			#process is still running
-			echo "Quiting at start, as pg_tailnmail process is still running : `cat $LOCK` " > $LOG
-			mailx -s "$BOX -> $PRG :: Last pg_tailnmail_process running. Call $Call " $MailError < $LOG
+			echo "Quiting at start, as pg-tailnmail process is still running : `cat $LOCK` " > $LOG
+			mailx -s "$BOX -> $PRG :: Last pg-tailnmail_process running. Call $Call " $MailError < $LOG
 			exit 1
 		else
 			#process is not running, but lock file was not deleted
 			echo "Process is not running, but Lock file not deleted. Deleting the lock file and quitting at start." > $LOG
-			mailx -s "$BOX -> $PRG :: pg_tailnmail removing lock file" $MailTo < $LOG
+			mailx -s "$BOX -> $PRG :: pg-tailnmail removing lock file" $MailTo < $LOG
 			rm -f $LOCK
 			exit 1
 		fi
@@ -27,7 +27,7 @@ if [ -f "/opt/vinay/dbops/pg_tailnmail/cfg/tnm.config.txt" ];then
 		echo $$ >> "$LOCK"
 		echo "done (pid=$$)"
 	
-		perl  /opt/vinay/dbops/pg_tailnmail/bin/pg_tailnmail.pl --verbose --pgmode=0 /opt/vinay/dbops/pg_tailnmail/cfg/tnm.config.txt
+		perl  /opt/vinay/dbops/pg-tailnmail/bin/pg-tailnmail.pl --verbose --pgmode=0 /opt/vinay/dbops/pg-tailnmail/cfg/tnm.config.txt
 	fi
 fi
 if [ -f "$LOCK" ];then
