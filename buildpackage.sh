3a94827120c41aea5d67bbd06c669aaacf70327f
Vinay Jaiswal <30997861+vinayjaiswal1990@users.noreply.github.com>
2020-12-30 23:40:56 +0530

    Update buildpackage.sh

--- buildpackage.sh
+++ buildpackage.sh
@@ -5,7 +5,7 @@ name="pg_tailnmail"
 version=$(grep Version $name/DEBIAN/control|awk '{print $2}')
 arch=$(grep Architecture $name/DEBIAN/control|awk '{print $2}')
 echo "Building package... $name version $version"
-dpkg-deb --build $name $name\_$version\_$arch.deb
+dpkg-deb --build $name $name\-$version\-$arch.deb
 if [ $? -eq 0 ]; then
     echo "Successfully built package"
 fi
